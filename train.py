29bf013e51980f25bb0f630a2f97a3c60135c6ab
burakcanbuyukbas <burakcanbuyukbas@hotmail.com>
2020-01-12 16:35:32 +0100

    train changes

--- train.py
+++ train.py
@@ -26,6 +26,7 @@ data_columns = ['cap-shape', 'cap-surface', 'cap-color', 'bruises', 'odor', 'gil
 Y = dataset['class']
 X = dataset.drop(['class'], axis=1)
 
+# encode string values as integers for data then use one-hot encoding
 
 encoded_X = None
 for i in range(0, X.shape[1]):
@@ -38,18 +39,19 @@ for i in range(0, X.shape[1]):
         encoded_X = feature
     else:
         encoded_X = np.concatenate((encoded_X, feature), axis=1)
-print("X shape: : ", encoded_X.shape)
-# encode string class values as integers
+
+# encode string values as integers for labels
 label_encoder = LabelEncoder()
 label_encoder = label_encoder.fit(Y)
 encoded_Y = label_encoder.transform(Y)
 
 
+# split train, validation and test datasets
 X_train, X_testval, Y_train, Y_testval = train_test_split(encoded_X, encoded_Y, test_size=0.2)
 X_test, X_val, Y_test, Y_val = train_test_split(X_testval, Y_testval, test_size=0.5)
 
 
-
+# shapes of data
 print("X_train.shape: " + str(X_train.shape))
 print("Y_train.shape: " + str(Y_train.shape))
 print("X_val.shape: " + str(X_val.shape))
@@ -57,13 +59,20 @@ print("Y_val.shape: " + str(Y_val.shape))
 print("X_test.shape: " + str(X_test.shape))
 print("Y_test.shape: " + str(Y_test.shape))
 
-
+# Implementation of the scikit-learn API for XGBoost classification.
 xg_model = XGBClassifier(n_estimators=1000, learning_rate=0.05)
+
+# Fit gradient boosting model
 xg_model.fit(X_train, Y_train, eval_set=[(X_val, Y_val)], verbose=False)
 
+# Predict with data
 test_preds = xg_model.predict(X_test)
 
+#round predictions
 predictions = [round(value) for value in test_preds]
+
 # evaluate predictions
 accuracy = accuracy_score(Y_test, predictions)
+
+#print accuracy
 print("Accuracy: %.2f%%" % (accuracy * 100.0))
